067d3f4e8c533e94b5669cde8b21d002303a7860
NDXDirectorsCut <Rares_sincu@yahoo.com>
2025-11-16 20:41:01 +0200

    Map

--- My project/Assets/Enigma/DefaultAction/FPSLookAction.cs
+++ My project/Assets/Enigma/DefaultAction/FPSLookAction.cs
@@ -57,12 +57,12 @@ namespace Enigma
         Vector2 angles;
         IEnumerator Turn(float turnX,float turnY)
         {
-            angles.x = Mathf.SmoothDamp(angles.x,turnX*turnSpeed,ref turnVelo.x,turnTime, 180, Time.fixedDeltaTime);
+            angles.x = Mathf.SmoothDamp(angles.x,turnX*turnSpeed,ref turnVelo.x,turnTime, 0, Time.deltaTime);
             Quaternion xRot = Quaternion.AngleAxis(angles.x,Vector3.up);
             transform.forward = 
                 xRot * transform.forward;
 
-            angles.y = Mathf.SmoothDamp(angles.y,-turnY*turnSpeed,ref turnVelo.y,turnTime, 180, Time.fixedDeltaTime);
+            angles.y = Mathf.SmoothDamp(angles.y,-turnY*turnSpeed,ref turnVelo.y,turnTime, 0, Time.deltaTime);
             Quaternion yRot = Quaternion.AngleAxis(angles.y,transform.right);
             transform.forward = yRot*transform.forward;
             
